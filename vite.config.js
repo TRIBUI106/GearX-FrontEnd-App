71a893552dff767b7cecf699d01fea665ed28913
chez1s <toptrainghieml@gmail.com>
2025-09-20 00:28:07 +0700

    base(init): the base structer of gearx vue project
    Add initial project files for a Vue 3 application using Vite, including configuration, main entry points, components, views, assets, routing, and state management setup. This provides a starting template with recommended tooling and structure.

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  
+  devServer: {
+    open: true, // Automatically opens the browser when the dev server starts
+    // openPage: '/dashboard', // Optional: specify a specific path to open
+  },
+  
+})
