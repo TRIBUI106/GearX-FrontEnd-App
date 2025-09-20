e1487da71aaad03cdf9f443fd786bbcb67ca2cfd
chez1s <toptrainghieml@gmail.com>
2025-09-20 22:36:38 +0700

    Add shadcn-vue UI components and login view
    Integrated a comprehensive set of shadcn-vue UI components, including form, navigation, and feedback elements. Added LoginForm and Login view, updated routing and main app structure, and configured project for new dependencies such as vee-validate, vue-sonner, and others.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), 
     vueDevTools(),
-    tailwindcss(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
